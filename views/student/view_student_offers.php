4146c9777ff67cbf84f23ed427904312ac1142e4
leonardo-mg <jessleomg@hotmail.com>
2023-12-14 04:19:15 -0600

    update css

---
+++ views/student/view_student_offers.php
@@ -0,0 +1,31 @@
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Krub:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/home.css">
+    <?php echo "<script>document.title = 'Ofertas';</script>"; ?>
+
+    <header>
+        <nav>
+            <ul>
+                <li><a href="home.html">Ofertas</a></li>
+                <li><a href="alumno.html">Mi Cuenta</a></li>
+                <li><a href="#">Cerrar Sesión</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="vacantes">
+            <h2>Vacantes</h2>
+            <div class="card">
+                <h3>Practicante Desarrollador Web</h3>
+                <p>Salario: $6,000</p>
+                <p>Ubicación: Tlajomulco de Zúñiga, Jal.</p>
+                <p>Sociedad Nacional Prom...</p>
+                <button>Postularse</button>
+            </div>
+            <!-- Más cards de vacantes... -->
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2023 Todos los derechos reservados</p>
+    </footer>
