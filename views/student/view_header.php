d447aeab9abb450d53d5c39a8958221724c261cc
DavisHrz <davishrz@hotmail.com>
2023-12-14 21:47:51 -0600

    update

---
+++ views/student/view_header.php
@@ -0,0 +1,9 @@
+<header>
+    <h1>Bienvenido, Estudiante</h1>
+    <nav>
+        <a href="index.html">Inicio</a>
+        <a href="home.html">Ofertas</a>
+        <a href="alumno.html">Mi cuenta</a>
+        <a href="?operation=1">Cerrar sesión</a>
+    </nav>
+</header>
