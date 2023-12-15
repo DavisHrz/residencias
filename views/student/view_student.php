6a5695654f11d0346e3c1ef9c9e03075f9efa461
leonardo-mg <jessleomg@hotmail.com>
2023-12-14 21:52:59 -0600

    update

--- views/student/view_student.php
+++ views/student/view_student.php
@@ -1,69 +1,47 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Krub:wght@400;700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="css/alumno.css">
+<link rel="stylesheet" href="css/perfilAlumno.css">
 <?php echo "<script>document.title = 'Portal Alumno';</script>"; ?>
 
 <header>
     <h1>Bienvenido, Estudiante</h1>
     <nav>
         <a href="index.html">Inicio</a>
-        <a href="home.html">Ofertas</a>
+        <a href="home.html">Proyectos</a>
         <a href="alumno.html">Mi cuenta</a>
+        <a href="alumnoPostulaciones.html">Mis postulaciones</a>
         <a href="?operation=1">Cerrar sesión</a>
     </nav>
 </header>
 
 <main>
-    <section class="perfil">
-        <h2>Crear Perfil de Alumno</h2>
-        <!-- Formulario para crear el perfil -->
-        <form id="perfilForm">
-            <label for="conocimientos">Conocimientos de software:</label>
-            <input type="text" id="conocimientos" name="conocimientos" required>
-
-            <label for="habilidades">Habilidades blandas:</label>
-            <input type="text" id="habilidades" name="habilidades" required>
-
-            <label for="promedio">Promedio:</label>
-            <input type="number" id="promedio" name="promedio" min="0" max="100" required>
-
-            <!-- Botón de enviar para crear el perfil -->
-            <button type="submit">Crear Perfil</button>
-        </form>
+    <section class="conocimientos">
+      <h2>Conocimientos de Software y Habilidades Blandas</h2>
+      <div class="info">
+        <p><strong>Software:</strong> HTML, CSS, JavaScript, Python</p>
+        <p><strong>Habilidades Blandas:</strong> Comunicación, Trabajo en equipo, Resolución de problemas</p>
+      </div>
+      <h2>Promedio</h2>
+      <div class="info">
+        <p><strong>Promedio:</strong> 90</p>
+      </div>
     </section>
+    <section class="evaluacion-residente">
+      <h2>Evaluación del Residente</h2>
+      <!-- Formulario para evaluar al residente al término de la residencia -->
+      <form id="evaluacionForm">
+        <label for="comportamiento">Comportamiento general sobre el proyecto:</label>
+        <textarea id="comportamiento" name="comportamiento" rows="4" required></textarea>
 
-    <section class="postulacion">
-        <h2>Postularse a Proyectos</h2>
-        <!-- Lista de proyectos disponibles -->
-        <ul>
-            <li>Proyecto 1</li>
-            <button class="postularBtn" data-proyecto="Proyecto 1">Postularse</button>
+        <label for="calificacionAceptacion">Calificación de Aceptación (1-100):</label>
+        <input type="number" id="calificacionAceptacion" name="calificacionAceptacion" min="1" max="100" required>
 
-            <li>Proyecto 2</li>
-            <button class="postularBtn" data-proyecto="Proyecto 2">Postularse</button>
-
-            <li>Proyecto 3</li>
-            <button class="postularBtn" data-proyecto="Proyecto 3">Postularse</button>
-        </ul>
+        <button type="submit">Enviar Evaluación</button>
+      </form>
     </section>
+  </main>
 
-    <section class="evaluacion">
-        <h2>Evaluación de la Empresa</h2>
-        <!-- Formulario para evaluar la empresa -->
-        <form id="evaluacionForm">
-            <label for="calificacion">Calificación (1-100):</label>
-            <input type="number" id="calificacion" name="calificacion" min="1" max="100" required>
-
-            <label for="experiencia">Experiencia con la empresa:</label>
-            <textarea id="experiencia" name="experiencia" rows="4" required></textarea>
-
-            <!-- Botón de enviar para evaluar -->
-            <button type="submit">Enviar Evaluación</button>
-        </form>
-    </section>
-</main>
-
-<footer>
-    <p>&copy; 2023 Portal de Alumnos</p>
-</footer>
+  <footer>
+    <p>&copy; 2023 Perfil de Alumno</p>
+  </footer>
